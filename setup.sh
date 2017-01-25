6a13625ffaafdb7f7ca44a0bcda45e02ba260591
Peter Bestler <peter.bestler@omicronenergy.com>
2017-01-25 14:13:32 +0100

    Adopt setup.sh we make the install independent from the call directory.
    Now we can clone the repo to everywhere, where we have access rights.
    Maintaining the git ignore is easier aswell.

--- setup.sh
+++ setup.sh
@@ -1,17 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Start installing config distro"
 
 echo "We remove current config"
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 rm -rf ${HOME}/.emacs.d ${HOME}/.bashrc ${HOME}/.zshrc ${HOME}/.vimrc ${HOME}/.Xdefaults ${HOME}/.screenrc
 
 echo "Setup links"
-ln -s ${HOME}/.config/emacs.d ${HOME}/.emacs.d
-ln -s ${HOME}/.config/commonconfigs/bashrc ${HOME}/.bashrc
-ln -s ${HOME}/.config/commonconfigs/zshrc ${HOME}/.zshrc
-ln -s ${HOME}/.config/commonconfigs/vimrc ${HOME}/.vimrc
-ln -s ${HOME}/.config/commonconfigs/Xdefaults ${HOME}/.Xdefaults
-ln -s ${HOME}/.config/commonconfigs/screenrc ${HOME}/.screenrc
+ln -s ${DIR}/emacs.d ${HOME}/.emacs.d
+ln -s ${DIR}/commonconfigs/bashrc ${HOME}/.bashrc
+ln -s ${DIR}/commonconfigs/zshrc ${HOME}/.zshrc
+ln -s ${DIR}/commonconfigs/vimrc ${HOME}/.vimrc
+ln -s ${DIR}/commonconfigs/Xdefaults ${HOME}/.Xdefaults
+ln -s ${DIR}/commonconfigs/screenrc ${HOME}/.screenrc
 
 echo "Installing complete"
