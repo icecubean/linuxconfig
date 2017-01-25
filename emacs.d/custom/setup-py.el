a101c7bfdbb7f6080171a60ba52cfd2e653263e5
Peter Bestler <peter.bestler@omicronenergy.com>
2017-01-25 13:51:24 +0100

    Improve python mode.

--- emacs.d/custom/setup-py.el
+++ emacs.d/custom/setup-py.el
@@ -1,8 +1,40 @@
 ;; company-c-headers
 (use-package company-jedi
+  :ensure t
+  :defer t
   :init
   (add-to-list 'company-backends 'company-jedi))
 
+(use-package pyvenv)
+
+(use-package auto-complete)
+
+; from python.el
+(use-package python
+  :ensure t
+  :defer t
+  :init
+  (require 'auto-complete-config)
+  (setq
+   python-shell-interpreter "ipython"
+   python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+   python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+   python-shell-completion-setup-code
+   "from IPython.core.completerlib import module_completion"
+   python-shell-completion-module-string-code
+   "';'.join(module_completion('''%s'''))\n"
+   python-shell-completion-string-code
+   "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+  (setq ac-sources '(ac-source-pycomplete
+                                ac-source-yasnippet
+                                ac-source-abbrev
+                                ac-source-dictionary
+                                ac-source-words-in-same-mode-buffers))
+  (ac-set-trigger-key "TAB")
+  (ac-set-trigger-key "<tab>")
+  (setq ac-auto-start 2)
+  (setq ac-ignore-case nil)
+  )
 
 
 (provide 'setup-py)
