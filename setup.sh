a1fa7aca228206eaebcb90612360af0b9fb395d3
Peter Bestler <peter.bestler@gmail.com>
2017-01-27 11:09:41 +0100

    Add brew to distro.

--- setup.sh
+++ setup.sh
@@ -6,15 +6,21 @@ echo "We remove current config"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-rm -rf ${HOME}/.emacs.d ${HOME}/.notion ${HOME}/.bashrc ${HOME}/.zshrc ${HOME}/.vimrc ${HOME}/.Xdefaults ${HOME}/.screenrc
+rm -rf ${HOME}/.emacs.d ${HOME}/.notion ${HOME}/.bashrc ${HOME}/.zshrc \
+   ${HOME}/.vimrc ${HOME}/.Xdefaults ${HOME}/.screenrc \
+   ${HOME}/.zprofile  ${HOME}/.brew
 
 echo "Setup links"
 ln -s ${DIR}/emacs.d ${HOME}/.emacs.d
 ln -s ${DIR}/notion ${HOME}/.notion
 ln -s ${DIR}/commonconfigs/bashrc ${HOME}/.bashrc
-ln -s ${DIR}/commonconfigs/zshrc ${HOME}/.zshrc
 ln -s ${DIR}/commonconfigs/vimrc ${HOME}/.vimrc
+ln -s ${DIR}/commonconfigs/zshrc ${HOME}/.zshrc
+ln -s ${DIR}/commonconfigs/zprofile ${HOME}/.zprofile
 ln -s ${DIR}/commonconfigs/Xdefaults ${HOME}/.Xdefaults
 ln -s ${DIR}/commonconfigs/screenrc ${HOME}/.screenrc
+
+# Setup brew
+ln -s ${DIR}/linuxbrew ${HOME}/.brew
 
 echo "Installing complete"
